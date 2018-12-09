4bef8376287fa19b3255af59ad34ae38c71e304a
Diogo Mariano <up201505876@fe.up.pt>
2018-12-09 17:32:22 +0100

    Final

--- app/src/main/java/com/example/user/homework2/MainActivity.java
+++ app/src/main/java/com/example/user/homework2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.user.homework2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -10,9 +12,20 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TASKS_FILE = "com.example.user.homework2.TasksFile";
+    public static final String NUM_TASKS = "NumOfTasks";
+    public static final String TASK ="task_";
+    public static final String ARTIST = "artist_";
+    public static final String RELEASE_DATE = "release date_";
+    public static final String PIC = "pic_";
 
     public static final String taskExtra = "Task";
     private int listItemPosition = -1;
@@ -33,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        restoreTasks();
 
         final TaskListFragment taskFr = (TaskListFragment) getSupportFragmentManager().findFragmentById(R.id.taskFragment);
         final FloatingActionButton FabDel = (FloatingActionButton) findViewById(R.id.deleteSong);
@@ -101,5 +115,69 @@ public class MainActivity extends AppCompatActivity {
 
         intent.putExtra(taskExtra, tmp);
         startActivity(intent);
+    }
+
+    private boolean saveTasks(){
+        SharedPreferences tasks = getSharedPreferences(TASKS_FILE, MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = tasks.edit();
+
+        editor.clear();
+
+        editor.putInt(NUM_TASKS, myTasks.size());
+
+        for(Integer i = 0; i<myTasks.size(); i++){
+            editor.putString(TASK + i.toString(), myTasks.get(i).Song_title);
+            editor.putString(ARTIST + i.toString(), myTasks.get(i).Artist_name);
+            editor.putString(RELEASE_DATE + i.toString(), myTasks.get(i).release_date);
+            editor.putString(PIC + i.toString(), myTasks.get(i).picPath);
+        }
+
+        return editor.commit();
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(!saveTasks()){
+            Toast.makeText(getApplicationContext(), "Save failed!", Toast.LENGTH_LONG).show();
+        }
+        saveTasksToFile();
+    }
+    private void restoreTasks(){
+        SharedPreferences tasks = getSharedPreferences(TASKS_FILE, MODE_PRIVATE);
+        int numOfTasks = tasks.getInt("NumOfTasks", 0);
+        if(numOfTasks!=0){
+            myTasks.clear();
+            for(Integer i=0; i<numOfTasks; i++){
+                String title = tasks.getString(TASK + i.toString(), "0");
+                String art = tasks.getString(ARTIST + i.toString(),"0");
+                String date = tasks.getString(RELEASE_DATE + i.toString(),"0");
+                String pic_path = tasks.getString(PIC + i.toString(),"");
+                Task tmp = new Task(title,art,date,pic_path);
+
+                myTasks.add(tmp);
+            }
+        }
+    }
+
+    private void saveTasksToFile(){
+        String filename = "myTasks.txt";
+        FileOutputStream outputStream;
+
+        try{
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputStream.getFD()));
+            String delim = ";";
+
+            for(Integer i=0; i<myTasks.size();i++){
+                Task tmp = myTasks.get(i);
+                String line = tmp.Song_title + delim + tmp.Artist_name + delim + tmp.release_date + delim + tmp.picPath;
+                writer.write(line);
+                writer.newLine();
+            }
+            writer.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/user/homework2/TaskInfoFragment.java
+++ app/src/main/java/com/example/user/homework2/TaskInfoFragment.java
@@ -7,7 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 
 /**
@@ -33,6 +36,29 @@ public class TaskInfoFragment extends Fragment {
         ((TextView)getActivity().findViewById(R.id.titleSongTextView)).setText(task.Song_title);
         ((TextView)getActivity().findViewById(R.id.ArtNameTextView)).setText(task.Artist_name);
         ((TextView)getActivity().findViewById(R.id.ageTextView)).setText(task.release_date);
+
+        ImageView imView = (ImageView)getActivity().findViewById(R.id.SPhoto);
+        Random rnd = new Random();
+        int a = rnd.nextInt(5);
+        switch (a)
+        {
+            case 0:
+                imView.setImageResource(R.drawable.et_logo);
+                break;
+            case 1:
+                imView.setImageResource(R.drawable.face);
+                break;
+            case 2:
+                imView.setImageResource(R.drawable.ic_launcher_background);
+                break;
+            case 3:
+                imView.setImageResource(R.drawable.pp_logo);
+                break;
+            case 4:
+                imView.setImageResource(R.drawable.ic_launcher_foreground);
+                break;
+        }
+
     }
 
     @Override
